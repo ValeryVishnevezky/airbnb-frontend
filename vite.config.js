dcb15a5a0b547b846fc97eb128d626663e9d8610
Valery Vishnevezky <valery020201@gmail.com>
2025-09-01 16:38:27 +0300

    Add infinite scroll

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: '../Airdnd.backend/public',
+		outDir: '../airbnb-backend/public',
 		emptyOutDir: true,
 	},
 })
